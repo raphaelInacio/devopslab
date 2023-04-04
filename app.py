17de9e7fd4db23e775335a65e0cf18ca79b1a216
Raphael Inacio <contato.raphaelinacio@gmail.com>
2023-04-04 07:45:07 -0300

    Habilitando Quality Gate

--- app.py
+++ app.py
@@ -10,6 +10,16 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Laboratório Pipeline DevOps"
 
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+
+
 if __name__ == '__main__':
     port = os.getenv('PORT')
     app.run('0.0.0.0', port=port)
